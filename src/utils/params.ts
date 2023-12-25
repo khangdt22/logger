edcf65401a8ffc213fddd71b509573a5747ab045
Diep Dang <khangdt310722@gmail.com>
2023-12-25 18:20:50 +0700

    feat(utils): add method to help easy generate log metadata

--- src/utils/params.ts
+++ src/utils/params.ts
@@ -1,5 +1,15 @@
+import type { AnyObject } from '@khangdt22/utils/object'
+import type { LogMetadata as BaseLogMetadata } from '../types'
+import { LOG_METADATA } from '../constants'
+
 export const LOG_PARAMS = Symbol.for('logger.params')
 
 export function params(input: () => any[]) {
     return { [LOG_PARAMS]: input }
+}
+
+type LogMetadata<T extends AnyObject = AnyObject> = Omit<BaseLogMetadata, typeof LOG_METADATA> & T
+
+export function metadata<T extends AnyObject>(input: LogMetadata<T>) {
+    return { [LOG_METADATA]: true, ...input }
 }
